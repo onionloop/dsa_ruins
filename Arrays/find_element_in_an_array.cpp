07333200408b658e621115ad5a6846af303b2dd3
onionloop <125755906+onionloop@users.noreply.github.com>
2025-11-16 00:42:37 +0530

    Initial commit

---
+++ Arrays/find_element_in_an_array.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+
+
+    int N , X;
+    cin >> N >> X;
+
+    vector <int> A(N);
+
+
+    for(int i = 0 ; i < N ; i++){
+        cin >> A[i];
+    }
+
+    bool found = false;
+
+
+    for(int i = 0 ; i < N ; i++){
+        if(A[i] == X){
+            found = true;
+            break;
+        }   
+    }
+
+    if(found){
+        cout << "YES" << endl;
+    }
+
+    else {
+        cout << "NO" << endl;
+    }
+
+
+    return 0;
+}
