44245d2297ad1a39eb7b682852314dc4a9469b2e
onionloop <125755906+onionloop@users.noreply.github.com>
2026-01-18 01:26:32 +0530

    Initial Commit

---
+++ recursion/recursion4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void func(int i ,int  n){
+
+    if (i < 1) return;
+    
+    cout << i << endl;
+    func( i-1 ,n);
+    
+}
+
+int main(){
+
+    
+    int n;
+    cin >> n;
+
+    func(n, 1);
+    
+}
+
+
+// backtracking!
