12ce2000432723e7fa2d350489233ee2e545971f
onionloop <125755906+onionloop@users.noreply.github.com>
2026-01-23 17:56:35 +0530

    Initial Commit

---
+++ hash/codechef/codechef/max_element.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector <int> arr(n);
+
+    for(int i = 0 ; i < n ; i++){
+        cin >> arr[i];
+    }
+
+    unordered_map<int , int> count;
+
+    for(int i =0 ; i < n ; i++){
+        count[arr[i]]++;
+    }
+
+    int max = 0;
+    int element;
+
+    for(auto ans : count){
+        if (ans.second > max) {
+            max = ans.second ;
+            element = ans.first;
+    }
+
+    
+
+}
+    cout << max << endl << element;
+    return 0;
+
+}
