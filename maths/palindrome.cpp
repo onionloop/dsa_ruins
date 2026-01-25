382d1b50123a06a41f580fc7d625ade61782fe1a
onionloop <125755906+onionloop@users.noreply.github.com>
2026-01-25 09:49:37 +0530

    Initial Commit

---
+++ maths/palindrome.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool palindrome(string s){
+
+    string newStr = "";
+
+    for(char c: s){
+        if (isalnum(c)) newStr += tolower(c);
+    }
+
+    return newStr == string(newStr.rbegin(), newStr.rend());
+}
+
+int main(){
+    string s;
+    cin >> s;
+
+    cout << palindrome(s) << endl;
+}
