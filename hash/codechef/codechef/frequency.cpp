960241b7a4f02dd02e6a722c48377f3b03be8c26
onionloop <125755906+onionloop@users.noreply.github.com>
2026-01-26 23:10:49 +0530

    Initial Commit

---
+++ hash/codechef/codechef/frequency.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector <int> arr(n);
+
+    for(int i = 0 ; i < n; i++){
+        cin >> arr[i];
+    }
+
+    unordered_map <int , int> freq;
+
+    
+}
