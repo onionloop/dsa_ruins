a94cc7da663b1c37b741269ef8ac4bac39040a68
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-01 18:43:50 +0530

    Initial Commit

---
+++ recursion/recursion13.cpp
@@ -0,0 +1,44 @@
+// Binary Search
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int bin(vector <int> &v, int low , int high, int target){
+
+    if (low > high) return -1;
+
+
+    int mid = (low + high)/2;
+
+    if (v[mid] == target) return mid;
+
+    else if (v[mid] > target) return bin(v , low , mid - 1 , target);
+
+    else  return bin(v , mid + 1 , high , target);
+    
+
+    return -1;
+
+}
+
+
+int main(){
+
+    int n; cin >> n;
+    vector <int> v(n);
+
+
+    int target;
+    cout << "Enter the target: ";
+    cin >> target;
+
+    for(int i =0 ; i < n ; i++){
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    cout << bin(v, 0 , v.size() - 1, target) << endl;
+}
