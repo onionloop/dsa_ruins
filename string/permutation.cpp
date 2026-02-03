4b7c992d5e94bf5fdff80dd28361b64b12315bf8
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-04 02:03:38 +0530

    Initial Commit

---
+++ string/permutation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    string s = "123";
+
+    do {
+        cout << s << endl;
+    } while(next_permutation(s.begin(), s.end()));
+
+    
+
+}
