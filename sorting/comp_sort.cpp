b705f4dbb8f99f9ec5052166da10dc477b8497a0
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-04 01:42:27 +0530

    Initial Commit

---
+++ sorting/comp_sort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool comp(pair <int , int> p1, pair<int , int> p2){
+    if(p1.second < p2.second) return true;
+    if(p1.second > p2.second) return false;
+
+    if(p1.first > p2.first) return true;
+}
+
+int main(){
+
+    pair <int , int> arr[] = {{1, 2} , {2, 1}, {4, 1}};
+
+
+}
