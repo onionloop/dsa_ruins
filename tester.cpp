84d80895945d1c4867736feb1445fc4fe13673fc
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-07 10:43:08 +0530

    Initial Commit

--- tester.cpp
+++ tester.cpp
@@ -2,12 +2,29 @@
 #include <vector>
 using namespace std;
 
-int selection(vector <int> &v , int n){
+void selection(vector <int> &v , int n){
 
     for(int i =0 ; i < n - 1 ; i++){
         int min = i;
-        for(int j = 0 ; j < n ; j++){
-            
+        for(int j = i + 1 ; j < n ; j++){
+            if(v[j] < v[min]) min = j;
         }
+        swap(v[i], v[min]);
     }
+
+    for(int i = 0 ; i < n ; i++){
+        cout << v[i] << " ";
+    }
+
+}
+
+int main(){
+    int n; cin >> n;
+    vector <int> v(n);
+
+    for(int i = 0; i < n ; i++){
+        cin >> v[i];
+    }
+
+    selection(v, n);
 }
