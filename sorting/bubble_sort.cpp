b7b86210110907cd68ae8e739dee18fa41c16e6f
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-07 12:03:42 +0530

    Initial Commit

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,22 @@
+// Pushes Maximum Element to the last
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+void bubble_sort(vector <int> &v, int n){
+    
+}
+
+
+
+int main(){
+    int n; cin >> n;
+    vector <int> v(n);
+
+    for(int i = 0; i < n ; i++){
+        cin >> v[i];
+    }
+
+    bubble_sort(v, n);
+}
--- sorting/comp_sort.cpp
+++ sorting/comp_sort.cpp
@@ -8,6 +8,8 @@ bool comp(pair <int , int> p1, pair<int , int> p2){
     if(p1.second > p2.second) return false;
 
     if(p1.first > p2.first) return true;
+
+    return false;
 }
 
 int main(){
