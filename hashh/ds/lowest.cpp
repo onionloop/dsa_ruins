aa55b86b383c5882bd14f8067e6686fdc460f16f
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-11 23:43:35 +0530

    Initial Commit

--- hashh/ds/lowest.cpp
+++ hashh/ds/lowest.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <climits>
-using namespace std;
+    #include <iostream>
+    #include <vector>
+    #include <unordered_map>
+    #include <climits>
+    using namespace std;
 
-int main(){
+    int main(){
 
-    int n;
-    cin >> n;
+        int n;
+        cin >> n;
 
-    vector <int> arr(n);
+        vector <int> arr(n);
 
-    for(int i =0 ;  i < n ; i++){
-        cin >> arr[i];
-    }
-
-    unordered_map <int , int> count;
-
-    for(int i = 0 ; i < n ; i ++){
-        count[arr[i]]++;
-    }
-
-
-    int minFreq = INT_MAX;
-    int element;
-
-    for(auto ans : count){
-        
-        
-        if (minFreq > ans.second){
-            minFreq = ans.second;
-            element = ans.first;    
-        
-
+        for(int i =0 ;  i < n ; i++){
+            cin >> arr[i];
         }
-    }
 
-    cout << "Min Appeared Element is : " << element << " " <<  "With the frequency of " << minFreq << endl;
-}
+        unordered_map <int , int> count;
+
+        for(int i = 0 ; i < n ; i ++){
+            count[arr[i]]++;
+        }
+
+
+        int minFreq = INT_MAX;
+        int element;
+
+        for(auto ans : count){
+            
+            
+            if (minFreq > ans.second){
+                minFreq = ans.second;
+                element = ans.first;    
+            
+
+            }
+        }
+
+        cout << "Min Appeared Element is : " << element << " " <<  "With the frequency of " << minFreq << endl;
+    }
