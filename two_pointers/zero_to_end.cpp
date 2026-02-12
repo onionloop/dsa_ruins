3796f78d455b6fc9c3d93f164d6aadffc417778b
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-12 23:39:40 +0530

    Initial Commit

--- two_pointers/zero_to_end.cpp
+++ two_pointers/zero_to_end.cpp
@@ -5,10 +5,36 @@ using namespace std;
 void moveZeros(vector <int> &v , int n){
     
     int j = 0;
-    
+
 
     for(int i =0 ; i < n ; i++){
-        v[j++] = v[i];
+        if(v[i] != 0) v[j++] = v[i];
+    }
+
+
+    while(j < n){
+        v[j++] = 0;
+    }
+
+    
+}
+
+
+int main(){
+    int n;
+    cin >> n;
+
+    vector <int> v(n);
+
+    for(int i =0 ; i < n ; i++){
+        cin >> v[i];
 
     }
+
+    moveZeros(v , n);
+
+    for(int i = 0; i < n; i++){
+        cout << v[i] << " ";
+    }
+
 }
