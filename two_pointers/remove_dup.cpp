e125bb97b5732af9dcb1495655ca6810719901e1
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-12 18:51:51 +0530

    Initial Commit

--- two_pointers/remove_dup.cpp
+++ two_pointers/remove_dup.cpp
@@ -4,16 +4,18 @@ using namespace std;
 
 void removeDup(vector <int> &v , int n){
     
+    if(n == 0 ) return ;
+
     int j = 0;
 
-    for(int i = 0 ; i < n ; i ++){
+    for(int i = 1 ; i < n ; i ++){
         if(v[i] != v[j]){
             j++;
             v[j] = v[i];
         }
     }
 
-    for(int i =0 ; i < n ; i++){
+    for(int i = 0; i < j + 1 ; i++){
         cout << v[i] << " ";
     }
 }
@@ -25,7 +27,7 @@ int main(){
 
     vector <int> v(n);
     for(int i =0 ; i < n ; i++){
-        cin >> n;
+        cin >> v[i];
     }
 
     removeDup(v , n);
