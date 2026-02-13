6be008c5240943ea5061173382815ca9cc5bda05
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-13 23:55:38 +0530

    Initial Commit

--- dp/tribonacci.cpp
+++ dp/tribonacci.cpp
@@ -4,5 +4,16 @@ using namespace std;
 
 int tribo(int n){
 
+    if(n <= 2) return n == 0 ? 0: 1;
+
+    vector<int> v(n);
+
+    v[0] = 0;
+    v[1] = 1;
+    v[2] = 1;
+    
+    for(int i =3 ; i < n ; i++){
+        v[i] = v[n - 1] + v[n - 2] + v[n -3];
+    }
 
 }
