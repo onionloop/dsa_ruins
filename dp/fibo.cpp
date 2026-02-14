2de424af6ab5cff430d484e986186cbe94a0f98d
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-14 23:36:43 +0530

    Initial Commit

--- dp/fibo.cpp
+++ dp/fibo.cpp
@@ -5,6 +5,24 @@ int fibo(int n){
 
     if( n <= 2) return n == 0 ? 0 : 1;
 
+    int first = 0;
+    int second = 1;
 
-    
+    for(int i =0 ; i < n ; i++){
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return second;
+
+}
+
+int main(){
+    int x;
+    cin >> x;
+
+    fibo(x);
+
+    return 0;
 }
