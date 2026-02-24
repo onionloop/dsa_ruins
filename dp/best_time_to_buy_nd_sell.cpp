ca66af820d88019c1495ffccc2575d6189e08bea
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-24 23:57:20 +0530

    Initial Commit

--- dp/best_time_to_buy_nd_sell.cpp
+++ dp/best_time_to_buy_nd_sell.cpp
@@ -1,2 +1,15 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
+
+int maxProfit(vector <int> &v){
+
+    int n = v.size();
+    int mini = v[0];
+    int maxP = 0;
+
+    for(int i =0 ; i < n ; i++){
+        int cost = prices[i] - mini;
+        maxP = 
+    }
+}
