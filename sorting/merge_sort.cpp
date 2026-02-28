7afeaa64a2ea6aaa2404ea96f24a7b0dcbbba784
onionloop <125755906+onionloop@users.noreply.github.com>
2026-02-28 11:11:59 +0530

    Initial Commit

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -16,11 +16,25 @@ void merge(vector<int> &v , int low, int mid , int high){
             left++;
         }
 
-        else (v[right] <= v[left]) {
+        else
+        {
             temp.push_back(v[right]);
-            right++
+            right++;
         }
+    }
 
+    while(left <= mid){
+        temp.push_back(v[left]);
+        left++;
+    }
+
+    while(right <= high){
+        temp.push_back(v[right]);
+        right++;
+    }
+
+    for(int i = low ; i <= high ; i++){
+        v[i] = temp[i-low];
     }
 }
 
