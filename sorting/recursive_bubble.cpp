c7c392a522a5020e6b6b01bc47bab7a9f1440433
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-03 06:28:25 +0530

    Initial Commit

--- sorting/recursive_bubble.cpp
+++ sorting/recursive_bubble.cpp
@@ -2,3 +2,38 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+void bubble(vector <int> &v, int n){
+
+    if (n == 1) return ;
+
+    int didSwap = 0;
+
+
+    for(int i = 0 ; i < n - 1; i++){
+        if(v[i] > v[i+1]) {
+            swap(v[i], v[i+1]);
+            didSwap = 1;
+        }
+    }
+
+    if (didSwap = 0) return;
+
+    bubble(v, n - 1);
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector <int> v(n);
+
+    for(int i =0 ; i < n ; i++){
+        cin >> v[i];
+    }
+
+    bubble(v, n);
+
+    return 0;
+}
