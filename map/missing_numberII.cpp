5d2f322593d8d2d04d700e03030e3cf6334ce1c1
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-05 19:57:24 +0530

    Initial Commit

---
+++ map/missing_numberII.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+
+int findMNum(vector <int> &v, int n){
+
+    
+}
+
+
+
+
+
+
+
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    vector <int> v(n);
+
+    for(int i= 0 ; i < n ; i++){
+        cin >> v[i];
+    }
+
+    cout << findMNum(v, n) << endl;
+
+    
+    return 0;
+}
