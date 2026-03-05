cf8ffbe66d63c036fba675bc830c4fd08e3eba5b
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-06 00:03:33 +0530

    Initial Commit

--- maths/missing_numberII.cpp
+++ maths/missing_numberII.cpp
@@ -2,10 +2,18 @@
 #include <vector>
 using namespace std;
 
+int missingNum(vector <int> &v, int n){
+
+    int total = (n*(n+1))/2;
+
+    int sum = 0;
 
 
+    for(int i = 0 ; i < v.size() ; i++) sum += v[i];
 
 
+    return total - sum;
+}
 
 
 int main(){
@@ -14,7 +22,13 @@ int main(){
     int n;
     cin >> n;
 
+    vector<int> v(n-1);
+
     for(int i = 0 ; i < n - 1; i++){
-        cin >> 
+        cin >> v[i]; 
     }
+
+    cout << missingNum(v, n) << endl;
+
+    return 0;
 }
