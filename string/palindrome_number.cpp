105498576c2906ca930b560e5a71c2fce0f656e9
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-07 01:42:06 +0530

    Initial Commit

---
+++ string/palindrome_number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool palN(int n){
+
+    string s = to_string(n);
+
+    string newStr = s;
+
+    reverse(newStr.begin(), newStr.end());
+
+    
+}
