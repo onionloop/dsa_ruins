722409b30d6d6acc503c70b1ac7c1217f89c3687
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-07 02:18:36 +0530

    Initial Commit

--- maths/lcm.cpp
+++ maths/lcm.cpp
@@ -22,6 +22,23 @@ int lcm(int x , int y){
     return (x*y)/gcd(x,y);
 }
 
+/*
+
+============================= WITHOUT GCD =============================
+
+int lcm(int a, int b){
+
+    int l = max(a , b);
+    int s = min(a, b);
+
+    for(int i = l; i <= a * b; l++){
+        if(i % s == 0) return i;
+    }
+
+}
+
+*/
+
 int main(){
     
     int a , b;
