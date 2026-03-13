362877fb9205ec86d4259845c4e864c90d7d4a58
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-14 01:32:56 +0530

    Initial Commit

--- multi_concept/3_sum.cpp
+++ multi_concept/3_sum.cpp
@@ -5,8 +5,40 @@
 #include <algorithm>
 using namespace std;
 
-int Tsum(vetor <int> &v){
+vector <int> Tsum(vector <int> &v){
 
-    sort()
+    sort(v.begin(), end());
+
+    vector <int> ans;
+
+    for(int i =0 ; i < v.size() ; i++){
+
+        if(i > 0  && v[i] == v[i-1]) continue;
+
+    }
+
+    int left = 0;
+    int right = v.size() - 1;
+
+    while(left < right){
+
+        int sum = v[i] + v[left] + v[right];
+
+        if(sum ==0 ){ 
+            
+        ans.push_back({v[i], v[left], v[right]});
+
+        left++;
+        right--;
+
+        while(left < right && v[i] == v[i+1]) left++;
+        while(left < right && v[i] == v[i - 1]) right--;
+    }
+
+        else if(sum < 0) left++;
+
+        else right ++;
+    }
 
 }
+
