f083ff0da3c00c2d6277081dd050fe0160771907
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-14 23:16:20 +0530

    Initial Commit

--- bits/int_to_binary.cpp
+++ bits/int_to_binary.cpp
@@ -3,5 +3,19 @@ using namespace std;
 
 string decTobinary(int n){
 
-    
+    int binary[32];
+
+    int i = 0;
+
+    int ans = 0;
+    while (n > 0){
+        binary[i] = n % 2;
+        n /= 2;
+        i++;
+    }
+
+
+    for (int j = i - 1; j >= 0 ; j--){
+        
+    }
 }
