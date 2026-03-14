df9d99d70201f88098dc945fece22a52238a26fb
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-14 23:41:16 +0530

    Initial Commit

--- maths/armstrong.cpp
+++ maths/armstrong.cpp
@@ -1,2 +1,30 @@
 #include <iostream>
 using namespace std;
+
+bool armstrong(int n){
+
+    string s = to_string(n);
+
+    int numbers = s.length();
+
+    int output = 0;
+
+    for(char c : s){
+
+        int ans = c - '0';
+
+        output += pow(ans , numbers);
+    }
+
+    return n == output;
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    cout << armstrong(n) << "\n";
+
+    return 0;
+}
