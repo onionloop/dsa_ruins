8433d0ef54658905377850c8a6bdddfc3572f312
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-15 23:45:00 +0530

    Initial Commit

--- algorithm/kadane.cpp
+++ algorithm/kadane.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <climits>
+#include <algorithm>
 using namespace std;
 
-int maxSubArray(int n){
+int maxSubArray(vector <int> &v){
 
-    
+    int sum =0;
+    int maxN = INT_MIN;
+    int n = v.size();
+
+    for(int i =0 ; i < n ;i++){
+        sum += v[i];
+        maxN = max(sum, maxN);
+
+        if(sum < 0) sum = 0;
+        
+    }
+
+    return maxN;
+}
+
+int main(){
+
+    int n; cin>> n;
+
+    vector <int> v(n);
+
+    for(int i =0 ; i< n ; i++){
+        cin >> v[i];
+    }
+
+    cout << maxSubArray(v);
+
+    return 0;
 }
