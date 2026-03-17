e1e03f8efaa4485080388ca361239173d8f308c2
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-17 23:18:51 +0530

    Initial Commit

--- maths/largest_prime_factor.cpp
+++ maths/largest_prime_factor.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+using namespace std;
 
 int largestPrimeF(int n){
 
-    
+    int largest = 0;
+
+    for(int i =0 ; i*i <= n ; i++){
+        
+        while(n % i == 0){
+            
+            largest = i;
+            n /= i;
+        }
+
+        if (n > 1) largest = n;
+    }
+
+    return largest;
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    cout << largestPrimeF(n) << endl;
+
+    return 0;
 }
