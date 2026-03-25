417e63ecc1e090f0a8590b75518d221687054795
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-25 23:33:25 +0530

    Initial Commit

--- bits/int_to_binary.cpp
+++ bits/int_to_binary.cpp
@@ -7,7 +7,7 @@ string decToBinary(int n){
     string ans = "";
     while(n > 0){
         int rem = n % 2;
-        ans += to_string(rem);
+        ans = to_string(rem) + ans;
         n /= 2;
     }
 
